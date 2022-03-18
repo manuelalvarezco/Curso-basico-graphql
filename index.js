1d58c421664345e5acc7c11d2037d67a2fee1aaf
Luis Manuel Alvarez <lalvarez@altimetrik.com>
2022-03-18 15:09:09 -0500

    unions pending and cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
+const cors = require('cors')
 
 const app = express();
 const port = process.env.port || 3000;
@@ -21,6 +22,8 @@ const schema = makeExecutableSchema({
   typeDefs,
   resolvers
 })
+
+app.use(cors())
 
 app.use('/api', graphqlHTTP({
   schema: schema,
